b60858098b6f91256a83871a271d1e3f7219fd81
morpheus <mariageorgepapas@gmail.com>
2020-03-17 00:12:40 -0500

    Challenge

--- src/com/mariageorgepapas/Main.java
+++ src/com/mariageorgepapas/Main.java
@@ -20,5 +20,28 @@ public class Main {
         System.out.println("MyIntValue = " + myIntValue);
         System.out.println("MyFloatValue = " + myFloatValue);
         System.out.println("MyDoubleValue = " + myDoubleValue);
+
+
+        // CHALLENGE: Convert a given number of pounds to kilograms
+        //
+        // STEPS:
+        // 1. Create a variable with the appropriate type to store the number of pounds to be converted to kilograms.
+        // 2. Calculate the result i.e. the number of kilograms based on the contents of the variable above and store
+        //    the result in a 2nd appropriate variable.
+        // 3. Print out the result.
+        //
+        // HINT: 1 pound is equal to 0.45359237 of a kilogram.
+
+        double numberOfPounds = 100d;
+        double poundToKilo = 0.45359237d;
+        double convertedKilograms = numberOfPounds * poundToKilo;
+        System.out.println("Converted kilograms = " + convertedKilograms);
+
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;           // different way of formatting a double with underscores
+        System.out.println(pi);
+        System.out.println(anotherNumber);
+
     }
 }
